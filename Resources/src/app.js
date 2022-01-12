9df1c92e00b0d9692f649cc4799426918e7ab54e
prashant31299 <chavanmadhav880@gmail.com>
2022-01-12 16:03:52 +0530

    adding html and js

---
+++ Resources/src/app.js
@@ -0,0 +1,8 @@
+let myblock;
+console.log("hallow wolrd");
+document.addEventListener("DOMContentLoaded",function(){
+    console.log("redy");
+    myblock=document.createElement("div")
+    myblock.textContent="hallow world bla bla";
+    document.body.appendChild(myblock)
+})
